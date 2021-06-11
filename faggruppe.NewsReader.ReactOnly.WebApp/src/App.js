6983afe07a9313ededc688a8cccf7284b3df94c3
Øistein Hay Syversen <oistein.hay.syversen@bekk.no>
2021-06-11 11:25:30 +0200

    Added admin files

--- faggruppe.NewsReader.ReactOnly.WebApp/src/App.js
+++ faggruppe.NewsReader.ReactOnly.WebApp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-// import { Home } from './domain/home/Home';
+import { Admin } from './domain/admin/Admin';
 // import { FetchData } from './components/FetchData';
 // import { Counter } from './components/Counter';
 import { NewsReader } from "./domain/news-reader/NewsReader";
@@ -15,7 +15,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={NewsReader} />
-        {/* <Route path='/newsreader' component={NewsReader} /> */}
+        <Route path='/admin' component={Admin} />
         {/* <Route path='/fetch-data' component={FetchData} /> */}
       </Layout>
     );
---
+++ faggruppe.NewsReader.ReactOnly.WebApp/src/domain/admin/Admin.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { NewsOutletMenu } from "../news-outlet-menu/NewsOutletMenu";
+import { groupByKey, findArrayElementByTag } from "../../components/Utils";
+
+export class Admin extends Component {
+  static displayName = Admin.name;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      outlets: [],
+      loadingOutlets: true,
+      selectedOutlet: "nrk", // Initial news outlet displayed
+    };
+
+    this.findArrayElementByTag = findArrayElementByTag.bind(this);
+    this.groupByKey = groupByKey.bind(this);
+    this.callbackFunction = this.callbackFunction.bind(this);
+
+    this.baseUrl = "http://newsapi.oh7.no";
+  }  
+
+  callbackFunction(selectedOutlet) {
+    this.setState({ selectedOutlet });
+  }
+
+  componentDidMount() {
+    this.getNewsOutlets();
+  }
+
+  renderNewsOutlets(outlets, selectedOutlet) {
+    const groupedOutlets = Object.entries(groupByKey(outlets, "group"));
+
+    return (
+        <div>
+          {groupedOutlets.map((data) => {
+            const title = data[0];
+            const outlets = data[1];
+            return (
+              <NewsOutletMenu
+                data={outlets}
+                selectedOutlet={selectedOutlet}
+                title={title}
+                callbackFunction={this.callbackFunction}
+                key={title}
+              />
+            );
+          })}
+        </div>
+    );
+  }
+
+  render() {
+    let newsOutletContent = this.state.loadingOutlets ? (
+      <p>
+        <em>Loading news outlets...</em>
+      </p>
+    ) : (
+      this.renderNewsOutlets(this.state.outlets, this.state.selectedOutlet)
+    );
+
+    return (
+      <div>
+        <div className="admin-container">{newsOutletContent}</div>
+      </div>
+    );
+  }
+
+  async getNewsOutlets() {
+    const url = `${this.baseUrl}/NewsOutletStore`;
+    await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((data) =>
+        this.setState({
+          outlets: data.newsOutlets,
+          loadingOutlets: false,
+        })
+      )
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
