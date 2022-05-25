e62f7aa346ab1351c48b822ce9b1b91e31560730
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2022-05-25 09:36:34 +0200

    Add react/bootstrap app

---
+++ src/faggruppe.newsreader.webapp/src/App.tsx
@@ -0,0 +1,80 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import './App.css';
+
+const Home = () => <span>Home</span>;
+
+const About = () => <span>About</span>;
+
+const Users = () => <span>Users</span>;
+
+const App = () => (
+  <MemoryRouter>
+    <Container className="p-3">
+      <Container className="p-5 mb-4 bg-light rounded-3">
+        <h1 className="header">Welcome To React-Bootstrap</h1>
+        <h2>
+          Current Page is{' '}
+          <Routes>
+            <Route path="/about" element={<About/>}>
+            </Route>
+            <Route path="/users" element={<Users/>}>
+            </Route>
+            <Route path="/" element={<Home/>}>
+            </Route>
+          </Routes>
+        </h2>
+        <h2>
+          Navigate to{' '}
+          <ButtonToolbar className="custom-btn-toolbar">
+            <LinkContainer to="/">
+              <Button>Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Button>About</Button>
+            </LinkContainer>
+            <LinkContainer to="/users">
+              <Button>Users</Button>
+            </LinkContainer>
+          </ButtonToolbar>
+        </h2>
+      </Container>
+    </Container>
+  </MemoryRouter>
+);
+
+export default App;
